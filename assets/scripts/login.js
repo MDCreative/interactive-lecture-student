f651f70586466b811854ba374544a3d44614ae8c
JamesJacko <james.ogge@gmail.com>
2015-03-11 10:18:36 +0000

    styling modal

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,32 +1,36 @@
 var app = angular.module("app", ["firebase"]);
 
 
+app.directive("login", function(){
+  return{
+    templateUrl: "assets/html/login.html",
+    restrict: "E",
+    controller: "LoginCtrl"
+  }
+});
+
 app.controller("LoginCtrl", function($scope, $firebase) {
 	$scope.signin = function(){
     //Firebase.goOnline();
     triad = new Triad($scope.id);   
     $scope.mainRef = new Firebase("https://interactive-lecture.firebaseio.com/Test/"+$scope.id);
+    var width = $( window ).width();
+    $('#login').animate({
+      left:"-"+ (width + 10) +"px"
+    }, 500);
 
     $scope.ref = $scope.mainRef.child('Questions');
-    
+    console.log("https://interactive-lecture.firebaseio.com/Test/"+$scope.id);
 
     var sync = $firebase($scope.ref);
     var syncObject = sync.$asObject();
     syncObject.$bindTo($scope, "data");
 
 
-    var conns = $scope.mainRef.child('connections');
-
     $scope.userRef = $scope.mainRef.child('users').push(1);
 
 
-    conns.transaction(function (current_value) {
-      return (current_value || 0) + 1;
-    });
-
     $scope.ref.limitToLast(1).on("child_added",function(messageSnapshot){
-
-
       var time = messageSnapshot.child('time').val();
       var type = messageSnapshot.child('type').val();
       var diff = Firebase.ServerValue.TIMESTAMP - parseInt(time);
@@ -65,6 +69,8 @@ app.controller("LoginCtrl", function($scope, $firebase) {
     }
 
   });
+
+
 
 
 
